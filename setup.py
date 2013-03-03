0670a069ca2480fa52bf1ec2e14dd644e849b441
dperetti <dperetti@gmail.com>
2013-03-03 16:02:49 +0100

    PiPy package name set to be "django_smartdbstorage".

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ from setuptools import find_packages
 
 
 setup(
-    name='smartdbstorage',
+    name='django_smartdbstorage',
     version='0.1.0',
     author='Dominique PERETTI',
     author_email='dperetti@lachoseinteractive.net',
     packages=find_packages(),
     #scripts=['bin/xx.py'],
-    url='http://pypi.python.org/pypi/SmartDBStorage/',
+    url='https://github.com/dperetti/Django-SmartDBStorage',
     license='LICENSE.txt',
     description='A Django Model based file storage.',
     long_description=open('README.txt').read(),
     install_requires=[
-        # "Django >= 1.1.1",
-        # "caldav == 0.1.4",
+        # "Django >= 1.4",
     ],
 )
