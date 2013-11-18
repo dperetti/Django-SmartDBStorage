cb94f4c9a4b66dd86609f8d2eaa2451f8bbf899c
dperetti <dperetti@gmail.com>
2013-11-18 22:41:24 +0100

    v0.1.1 Django 1.6 compatibility.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django_smartdbstorage',
-    version='0.1.0',
+    version='0.1.1',
     author='Dominique PERETTI',
     author_email='dperetti@lachoseinteractive.net',
     packages=find_packages(),
--- smartdbstorage/urls.py
+++ smartdbstorage/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls.defaults import patterns, include, url
+import django
+if django.VERSION[:2] < (1, 6):
+    from django.conf.urls.defaults import patterns, include, url
+else:
+    from django.conf.urls import patterns, include, url
 from smartdbstorage.views import DBFileView
 
 urlpatterns = patterns('',
     #url(r'^file/(?P<pk>\d+)/$', DBFileView.as_view(), name="dbfile_view")
     url(r'^file/(?P<prefix>[^/]+)/(?P<name>.*)$', DBFileView.as_view(), name="file")
-
 )
