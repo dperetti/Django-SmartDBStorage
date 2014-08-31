21005bc2d3388cc27407de496a82a096cfd0b918
Dominique PERETTI <dperetti@gmail.com>
2014-08-31 19:52:06 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django_smartdbstorage',
-    version='0.1.1',
+    version='0.2',
     author='Dominique PERETTI',
     author_email='dperetti@lachoseinteractive.net',
     packages=find_packages(),
@@ -14,6 +14,6 @@ setup(
     description='A Django Model based file storage.',
     long_description=open('README.txt').read(),
     install_requires=[
-        'South>=0.7.3'
+        'Django>=1.7'
     ],
 )
